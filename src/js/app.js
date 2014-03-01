87e01e0a44a2d46a9624ee922029da2ea0b39870
Hank Yates <hankyates@gmail.com>
2014-03-01 04:57:23 +0000

    spike is over. let the building begin!

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ define(['lodash'], function(_) {
     var projectCards = document.getElementsByClassName('project-card');
     _.each(projectCards, function(card) {
       card.addEventListener('click', function() {
-        var openElement = document.getElementsByClassName('open')[0]
+        var openElement = document.getElementsByClassName('open')[0];
         if(openElement)
           openElement.classList.remove('open');
         this.classList.add('open');
